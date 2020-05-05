7941c2a3b27fc04ba3e454c9d206f1e66a2ed5ca
Aryaman Gupta <aryaman.iitbhu@gmail.com>
2020-05-06 00:33:14 +0530

    change

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render,redirect    
 from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.forms import UserCreationForm
-from authentication.forms import SignUpForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -17,7 +16,7 @@ def loginView(request):
             login(request,user)
             return redirect('/')
         else:
-            return HttpResponse("Invalid login details given")
+            return render(request, 'registration/login.html', {'error':"Invalid UserName or Password"})
     else:
         return render(request, 'registration/login.html', {})
 @login_required
@@ -27,15 +26,34 @@ def logoutView(request):
 
 def registerView(request):
     if request.method=="POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+        try:
+            username=request.POST['username']
+            password1=request.POST['password1']
+            password2=request.POST['password2']
+            firstname=request.POST['firstname']
+            lastname=request.POST['lastname']
+            email=request.POST['email']
+        except:
+            return render(request,'registration/register.html',{'msg':"Fill All The Fields"})
+        if password1 != password2:
+            return render(request, 'registration/register.html', { 'msg': "Passwords not matching" })
+        try:
+            user = User.objects.create_user(
+                username = username,
+                first_name = firstname,
+                last_name = lastname,
+                email = email,
+                password = password1
+            )
+            user.save()
+        except:
+            return render(request, 'registration/register.html', { 'msg': "Username already exists" })
+        user = authenticate(request, username=username, password=password1)
+        if user is not None:
             login(request, user)
             return redirect('/')
+        else:
+            return render(request, 'registration/register.html', { 'msg': "User created Successfully" })
     else:
-        form = SignUpForm()
-    return render(request, 'registration/register.html', {'form': form})
+        return render(request, 'registration/register.html')
 
